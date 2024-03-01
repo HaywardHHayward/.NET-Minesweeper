2e41014d487b2962e59f28a89ce4593642e130e9
HaywardHHayward <tictanic8@hotmail.com>
2024-02-29 19:59:25 -0600

    Added help command argument

--- Program.cs
+++ Program.cs
@@ -7,6 +7,24 @@ public static partial class Program {
     }
 
     public static void Main(string[] args) {
+        if (args.Length == 1 && (args[0] == "--help" || args[0] == "--h")) {
+            Console.WriteLine("""
+                              Minesweeper help:
+                                    .../Minesweeper : Runs the program, gets user inputs from the application itself
+                                    .../Minesweeper --h(elp) : Outputs information about command line arguments
+                                    .../Minesweeper [r] [c] [m] : Runs the program with the specified board inputs, gets color and interface inputs from application
+                                        [r] : The amount of rows in the board, must be a positive integer
+                                        [c] : The amount of columns in the board, must be a positive integer
+                                        [m] : The amount of mines in the board, must be a positive integer less than [r] * [c] - 8
+                                    .../Minesweeper [r] [c] [m] [color] [inter] : Immediately starts a game of Minesweeper with the specified inputs
+                                        [r]     : The amount of rows in the board, must be a positive integer
+                                        [c]     : The amount of columns in the board, must be a positive integer
+                                        [m]     : The amount of mines in the board, must be less than [r] * [c] - 8
+                                        [color] : Either "colored" or "monochrome"
+                                        [inter] : Either "keyboard" or "text"
+                              """);
+            return;
+        }
         Console.Clear();
         try {
             if (args.Length != 0) {
@@ -27,7 +45,8 @@ public static partial class Program {
         }
         catch (Exception e) {
             Console.Clear();
-            Console.WriteLine($"Minesweeper has experienced a fatal error. Error: {e.Message}\nMinesweeper will now exit following any key press...");
+            Console.WriteLine(
+                $"Minesweeper has experienced a fatal error. Error: {e.Message}\nMinesweeper will now exit following any key press...");
             Console.ReadKey();
             return;
         }
