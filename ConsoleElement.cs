b7f262ce1809272e90c06c11fbe1791e90ca3ba6
HaywardHHayward <tictanic8@hotmail.com>
2024-03-11 14:27:54 -0500

    Moved ConsoleElement to its own file

---
+++ ConsoleElement.cs
@@ -0,0 +1,15 @@
+namespace Minesweeper;
+
+internal readonly struct ConsoleElement(int x, int y, string message) {
+    public string Message { get; } = message;
+    public int ElementX { get; } = x;
+    public int ElementY { get; } = y;
+}
+
+internal readonly struct ConsoleElement<T>(int x, int y, T obj, ConsoleElement<T>.StringDelegate method) {
+    public delegate string StringDelegate(T obj);
+
+    public int ElementX { get; } = x;
+    public int ElementY { get; } = y;
+    public string Message => method(obj);
+}
